3d28fe3085d0531e72ca8d627c88acf40ac7c8b3
skillet-sys <kaito.goforward0912@gmail.com>
2021-10-23 01:40:16 +0900

    ラズパイのCPU温度

---
+++ measure_temp.py
@@ -0,0 +1,19 @@
+import subprocess
+
+import notice
+
+
+def get_temp():
+    temp = subprocess.getoutput('vcgencmd measure_temp').split('=')
+    message = "\n現在のRaspberry PiのCPU温度 : "+temp[1].replace('\'', '°')
+
+    return message
+
+
+def main():
+    message = get_temp()
+    notice.notice()
+
+
+if __name__ == '__main__':
+    main()

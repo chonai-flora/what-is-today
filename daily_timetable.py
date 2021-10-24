9e553e29c5c235ccbc95e1441a3abb1154ba16b8
skillet-sys <kaito.goforward0912@gmail.com>
2021-10-24 13:24:05 +0900

    時間割

--- daily_timetable.py
+++ daily_timetable.py
@@ -7,12 +7,14 @@ import notice
 def get_timetable():
     weekday = datetime.now().weekday()
     file_name = open('data/timetable.csv', 'r')
-    timetable = list(csv.reader(file_name))
+    timetable_file = list(csv.reader(file_name))
 
     if weekday > 4:
         return "\n今日は休暇です"
 
-    message = "\n今日の時間割\n"+"\n".join(timetable[weekday])
+    timetable = ["{0}限 : {1}\n".format(
+        i+1, (timetable_file[weekday][i])) for i in range(5)]
+    message = "\n今日の時間割\n"+"".join(timetable)
     return message
 
 
